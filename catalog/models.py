33d65948813d0d120dd8b298c7849c4f3ba3ee41
DESKTOP-VLJ0N4A\김종백 <good5229@naver.com>
2018-01-30 17:38:09 +0900

    feat(library):add admin
    https://app.asana.com/0/513674312489999/538057853929359

--- catalog/models.py
+++ catalog/models.py
@@ -17,6 +17,18 @@ class Genre(models.Model):
         """
         return self.name
 
+class Language(models.Model):
+    """
+    Model representing a Language (e.g. English, French, Japanese, etc.)
+    """
+    name = models.CharField(max_length=200, help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)")
+    
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
 class Book(models.Model):
     """
     Model representing a book (but not a specific copy of a book).
@@ -43,6 +55,13 @@ class Book(models.Model):
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
     """
